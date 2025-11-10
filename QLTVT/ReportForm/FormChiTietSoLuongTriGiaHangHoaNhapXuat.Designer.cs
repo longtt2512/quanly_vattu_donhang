a6a9b9d907c14e29620c0837e585ee5d6b40b650
phandung2101 <dungpv.dev@gmail.com>
2025-11-10 09:23:53 +0700

    Fix chi tiet nhap xuat

--- QLTVT/ReportForm/FormChiTietSoLuongTriGiaHangHoaNhapXuat.Designer.cs
+++ QLTVT/ReportForm/FormChiTietSoLuongTriGiaHangHoaNhapXuat.Designer.cs
@@ -36,7 +36,6 @@ namespace QLTVT.ReportForm
             this.label4 = new System.Windows.Forms.Label();
             this.dteTuNgay = new DevExpress.XtraEditors.DateEdit();
             this.dteToiNgay = new DevExpress.XtraEditors.DateEdit();
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dteTuNgay.Properties.CalendarTimeProperties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dteTuNgay.Properties)).BeginInit();
@@ -46,13 +45,16 @@ namespace QLTVT.ReportForm
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(46, 46);
+            this.label1.AutoSize = false;
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Tahoma", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 60);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(819, 40);
+            this.label1.Size = new System.Drawing.Size(871, 41);
             this.label1.TabIndex = 0;
             this.label1.Text = "CHI TIẾT SỐ LƯỢNG HÀNG HÓA NHẬP XUẤT";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label2
             // 
@@ -122,30 +124,16 @@ namespace QLTVT.ReportForm
             this.dteToiNgay.Size = new System.Drawing.Size(156, 24);
             this.dteToiNgay.TabIndex = 6;
             // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.Blue;
-            this.button1.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(137, 268);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(184, 46);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "XEM TRƯỚC";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // button2
             // 
-            this.button2.BackColor = System.Drawing.Color.Red;
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(530, 268);
+            this.button2.Location = new System.Drawing.Point(15, 15);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(184, 46);
-            this.button2.TabIndex = 8;
-            this.button2.Text = "XUẤT BẢN";
-            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Size = new System.Drawing.Size(100, 27);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Xuất file";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)));
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // FormChiTietSoLuongTriGiaHangHoaNhapXuat
@@ -154,7 +142,6 @@ namespace QLTVT.ReportForm
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(901, 374);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.dteToiNgay);
             this.Controls.Add(this.dteTuNgay);
             this.Controls.Add(this.label4);
@@ -162,7 +149,6 @@ namespace QLTVT.ReportForm
             this.Controls.Add(this.cmbLoaiPhieu);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "FormChiTietSoLuongTriGiaHangHoaNhapXuat";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Chi Tiết Số Lượng Hàng Hóa Nhập Xuất";
@@ -185,7 +171,6 @@ namespace QLTVT.ReportForm
         private System.Windows.Forms.Label label4;
         private DevExpress.XtraEditors.DateEdit dteTuNgay;
         private DevExpress.XtraEditors.DateEdit dteToiNgay;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
     }
 }
